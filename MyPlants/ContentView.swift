f067cf9777920a4218ac366ab3a40ccb90a3a205
Ghada <ghada-alshabanat@hotmail.com>
2024-10-29 10:37:03 +0300

    Initial Commit

---
+++ MyPlants/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPlants
+//
+//  Created by Ghada Alshabanat on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPlants/MyPlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlantsApp.swift
+//  MyPlants
+//
+//  Created by Ghada Alshabanat on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyPlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
